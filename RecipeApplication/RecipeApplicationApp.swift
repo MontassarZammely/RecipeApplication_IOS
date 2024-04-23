4f0057f69fe276f1a40b8033f2666eefcc61801b
MontassarZammely <Montazammel10@gmail.com>
2024-04-23 15:04:26 +0100

    Initial Commit

---
+++ RecipeApplication/RecipeApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApplicationApp.swift
+//  RecipeApplication
+//
+//  Created by Montassar Ben Zammel on 23/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
